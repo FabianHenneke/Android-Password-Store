e954b670edf9a5d08e1d23ea102794cc43b3b86e
Fabian Henneke <fabian@henneke.me>
2020-02-19 17:13:29 +0100

    Make package verification ID computation stable

--- app/src/main/java/com/zeapo/pwdstore/autofill/oreo/VerificationHelper.kt
+++ app/src/main/java/com/zeapo/pwdstore/autofill/oreo/VerificationHelper.kt
@@ -12,6 +12,10 @@ private fun ByteArray.sha256(): ByteArray {
     }
 }
 
+private fun ByteArray.base64(): String {
+    return Base64.encodeToString(this, Base64.NO_WRAP)
+}
+
 private fun combineHashes(hashes: List<ByteArray>): ByteArray {
     return MessageDigest.getInstance("SHA-256").run {
         for (hash in hashes)
@@ -20,9 +24,20 @@ private fun combineHashes(hashes: List<ByteArray>): ByteArray {
     }
 }
 
+object LexicographicCompare : Comparator<ByteArray> {
+    override fun compare(a: ByteArray, b: ByteArray): Int {
+        val minLength = a.size.coerceAtMost(b.size)
+        for (i in 0 until minLength) {
+            if (a[i] != b[i])
+                return a[i].toInt() - b[i].toInt()
+        }
+        return a.size - b.size
+    }
+}
+
 fun Context.getPackageVerificationId(packageName: String): String? {
     val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
     val signatureHashes = info.signatures.map { it.toByteArray().sha256() }
-    val fullHash = combineHashes(signatureHashes)
-    return Base64.encodeToString(fullHash, Base64.NO_WRAP)
+    val fullHash = combineHashes(signatureHashes.sortedWith(LexicographicCompare))
+    return fullHash.base64()
 }
